6d01959f7d7850d405831fb7b5e3980cfefb416a
Maximilian Schneider <mail@maximilianschneider.net>
2024-09-23 17:30:57 +0800

    jito tip & priority fee indexing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { AddressLookupTableAccount, ComputeBudgetInstruction, ComputeBudgetProgram, Connection, PublicKey } from "@solana/web3.js";
+import * as lo from "@solana/buffer-layout";
 
 const prisma = new PrismaClient();
 dotenv.config();
@@ -25,32 +26,85 @@ async function getLastSlot() {
   }
 }
 
+const tipPaymentAddresses = [
+  "96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5",
+  "HFqU5x63VTqvQss8hp11i4wVV8bD44PvwucfZ2bU7gRe",
+  "Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY",
+  "ADaUMid9yfUytqMBgopwjb2DTLSokTSzL1zt6iGPaS49",
+  "DfXygSm4jCyNCybVYYK6DwvWqjKee8pbDmJGcLWNDXjh",
+  "ADuUkR4vqLUMWXxW9gh6D6L8pMSawimctcNZ5pGwDcEt",
+  "DttWaMuVvTiduZRnguLF7jNxTgiMBZ1hyAumKUiL2KRL",
+  "3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT",
+];
+
+
+const altCache: {[pk: string]: AddressLookupTableAccount} = {};
+
+
 async function indexBlock(slot: number) {
   try {
     const block = await conn.getBlock(slot, { commitment, maxSupportedTransactionVersion: 1, rewards: true, transactionDetails: "full" });
     const leader = block!.rewards!.find((r) => r.rewardType == "Fee")!.pubkey;
 
-    let nonVoteTxs = block!.transactions.filter(
+    const nonVoteTxs = block!.transactions.filter(
       (tx) => !tx.transaction.message.compiledInstructions.find(i => tx.transaction.message.staticAccountKeys[i.programIdIndex].equals(votePk))
     );
 
-    const uniqueAccountKeys = new Set(
-      nonVoteTxs
-        .map(tx => tx.transaction.message.getAccountKeys({accountKeysFromLookups: tx.meta?.loadedAddresses}).keySegments())
-        .flat(2)
-        .map(pk => pk.toString())
+    const alts = new Set(nonVoteTxs.flatMap(tx => tx.transaction.message.addressTableLookups.map(alt => alt.accountKey)));
+    const altPks = [...alts.keys()];
+
+    // update ALT cache
+    const newAltPks = altPks.filter(pk => !altCache[pk.toString()]);
+    const newAltAis = await conn.getMultipleAccountsInfo(newAltPks);
+    newAltPks.forEach(
+      (key, i) => {
+        let alt = new AddressLookupTableAccount({
+          key,
+          state: AddressLookupTableAccount.deserialize(newAltAis[i]!.data),
+        })
+        altCache[key.toString()] = alt;
+      }
     );
 
-    const txToCreate = nonVoteTxs.map((tx) => ({
-      hash: tx.transaction.signatures[0],
-      slotId: slot,
-      CUConsumed: tx.meta?.computeUnitsConsumed!,
-      CURequested: tx.transaction.message.compiledInstructions.length * 200000,
-    }));
 
-    const addrsToCreate = Array.from(uniqueAccountKeys.values())
-      .map((a) => ({ address: a }))
-      .concat([{ address: leader }]);
+    const txToCreate = nonVoteTxs.map((tx) => {
+      const hash = tx.transaction.signatures[0];
+      const CUConsumed = tx.meta?.computeUnitsConsumed!;
+      const CURequested = tx.transaction.message.compiledInstructions.length * 200000;
+
+      // decode account keys
+      const addressLookupTableAccounts = tx.transaction.message.addressTableLookups.map(alt => altCache[alt.accountKey.toString()]);
+      const accountKeys = tx.transaction.message.getAccountKeys({ addressLookupTableAccounts }).keySegments().flat();
+
+      // calculate jito tip
+      let TipPaid = 0;
+      for (let i = 0; i < accountKeys.length; ++i) {
+        const address = accountKeys[i].toString();
+        if (tipPaymentAddresses.includes(address)) {
+          TipPaid = tx.meta!.postBalances[i] - tx.meta!.preBalances[i];
+        }
+      }
+
+      // calculate priorioty fee
+      let PriorityPaid = 0;
+      const computeBudgetIndex = accountKeys.findIndex(k => k.equals(ComputeBudgetProgram.programId));
+      const computeLimitIx = tx.transaction.message.compiledInstructions.find(ix => ix.programIdIndex == computeBudgetIndex && ix.data[0] == 3);
+      if (computeLimitIx) {
+        PriorityPaid = lo.nu64().decode(computeLimitIx.data, 1)
+      }
+
+      return {
+        hash,
+        slotId: slot,
+        CUConsumed,
+        CURequested,
+        TipPaid,
+        PriorityPaid,
+      };
+    }
+    );
+
+    const addrsToCreate = [{ address: leader }];
 
     await Promise.all([
       prisma.transaction.createMany({ data: txToCreate, skipDuplicates: true }),
@@ -59,48 +113,16 @@ async function indexBlock(slot: number) {
         skipDuplicates: true,
       }),
     ]);
-    const [txIds, addressIds] = await Promise.all([
-      prisma.transaction.findMany({
-        select: { id: true, hash: true },
-        where: {
-          hash: {
-            in: nonVoteTxs.map((tx) => tx.transaction.signatures[0]),
-          },
-        },
-      }),
 
-      prisma.accountAddress.findMany({
-        where: { address: { in: Array.from(uniqueAccountKeys.values()) } },
-      }),
-    ]);
-    const idByAddress = Object.fromEntries(
-      addressIds.map((aid) => [aid.address, aid.id])
-    );
-
-    let recordedAccounts = [];
-    for (let tx of nonVoteTxs) {
-      const { id: txId } = txIds.find(
-        (txid) => txid.hash == tx.transaction.signatures[0]
-      )!;
-      const programIds = tx.transaction.message.compiledInstructions.map(i => tx.transaction.message.staticAccountKeys[i.programIdIndex]);
-      const accountKeys = tx.transaction.message.getAccountKeys({accountKeysFromLookups: tx.meta?.loadedAddresses}).keySegments().flat(1);
-      recordedAccounts.push(...accountKeys.map((k, i) => ({
-        txId,
-        addressId: idByAddress[k.toString()],
-        flags: (tx.transaction.message.isAccountWritable(i) ? 1 : 0) + (tx.transaction.message.isAccountSigner(i) ? 4 : programIds.includes(k) ? 8 : 0)
-      })));
-    }
-
-    await prisma.accountKey.createMany({
-      data: recordedAccounts,
-      skipDuplicates: true,
+    const leaderId = await prisma.accountAddress.findFirst({
+      where: { address: { equals: leader } },
     });
 
     await prisma.slot.create({
       data: {
         id: slot,
         parentId: block!.parentSlot,
-        leaderId: idByAddress[leader],
+        leaderId: leaderId!.id,
         blockTime: new Date(block!.blockTime! * 1000),
       },
     });
@@ -115,10 +137,6 @@ async function indexBlock(slot: number) {
       block?.transactions.length,
       "nonVoteTxs:",
       nonVoteTxs.length,
-      "uniqueAccounts:",
-      uniqueAccountKeys.size,
-      "accountInfos:",
-      recordedAccounts.length,
       "compute:",
       txToCreate.reduce((prev, current) => prev + current.CUConsumed, 0)
     );
@@ -146,13 +164,13 @@ async function main() {
   console.log("fetch slots", newSlots);
 
 
-  // for (let s of newSlots) {
-  //   await indexBlock(s);
-  // }
-  await eachLimit(
-    newSlots.map((s) => indexBlock(s)),
-    5
-  );
+  for (let s of newSlots) {
+    await indexBlock(s);
+  }
+  // await eachLimit(
+  //   newSlots.map((s) => indexBlock(s)),
+  //   5
+  // );
 
   main();
 }
